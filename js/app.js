9cb004a6f77d28596149b4357a2fb4f13a1df444
chadwyck242 <chadwyck.codeart@gmail.com>
2018-06-04 21:18:28 -0600

    Add random sprite generation for Enemy class

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
+// MDN JavaScript Reference
+// Math.random() inclusive min and max example function:
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 // Enemies our player must avoid
-var Enemy = function(sprite, x, y) {
+var Enemy = function(sprite, x, y, speed) {
 
     // loads the sprite image
     this.sprite = 'images/enemy-bug.png';
     // sets location of Enemy sprite
     this.x = x;
     this.y = y;
+    this.speed =  speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,7 +23,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    x += x * dt;
+    if (allEnemies.length <= 3) {
+        allEnemies.push({
+            sprite: sprite,
+            x: getRandomInt(3) * 101 - canvas.width,
+            y: canvas.height - getRandomIntInclusive(3, 5) * 171,
+            speed: x + getRandomIntInclusive(1, 5) * 10 * dt
+        });
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,27 +41,20 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(sprite, x, y) {
+var Player = function(sprite, x, y, speed) {
     // loads the sprite image
     this.sprite = 'images/char-cat-girl.png';
-    // sets location of Enemy sprite
+    // sets location of Player sprite
     this.x = x;
     this.y = y;
+    this.speed = speed;
 };
 
 // Now instantiate your objects.
-
-var player = new Player(sprite, x, y);
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [];
-player = {
-    sprite: sprite,
-    x: 105,
-    y: 90
-};
-
-
+// Place the player object in a variable called player
+var player = new Player(this.sprite, 100, 100, 10);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
